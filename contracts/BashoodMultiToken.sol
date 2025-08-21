42398d6a765a6539f8ddf079395645e0108fd081
franchu <franchu@example.local>
2025-08-22 01:20:41 +0200

    fix: update OZ import paths to v5 layout (utils)

--- contracts/BashoodMultiToken.sol
+++ contracts/BashoodMultiToken.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.28;
  
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
  
--- contracts/BashoodPresaleFinal.sol
+++ contracts/BashoodPresaleFinal.sol
@@ -9,8 +9,8 @@ pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./AggregatorV3Interface.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/Pausable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
