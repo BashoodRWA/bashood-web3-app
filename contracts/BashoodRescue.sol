34d8477fe308faa4d89ae3ae6cd8f3650278ab4a
franchu <franchu@example.local>
2025-09-21 16:22:30 +0200

    fix(release): correct ReentrancyGuard import path

--- contracts/BashoodRescue.sol
+++ contracts/BashoodRescue.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
 import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
